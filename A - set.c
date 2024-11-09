f72a68a767f8c2a762a4b99e4e26b0df4592a578
sngr0x0 <136003366+Frances-co@users.noreply.github.com>
2024-11-09 18:18:22 +0200

    Update A - set.c

--- A - set.c
+++ A - set.c
@@ -28,3 +28,29 @@ int main(){
     
     return 0;
 }
+
+//This code above caused a time-limit exceed.. I spent about 1:34 hours trying to figure out a formula (on my own.. no search or chatGPT) and I came out with this
+//soln which passed the 4 pretests
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int ncases;
+    int l, r, k;
+    scanf("%d", &ncases);
+    
+    int operations=0;
+    
+    while(ncases--){
+        scanf("%d %d %d", &l, &r, &k);
+        operations= (r/k) - l + 1;
+        if(operations<0){
+            printf("%d\n", 0);
+        }
+        else
+            printf("%d\n", operations);
+    }    
+    
+    return 0;
+}
