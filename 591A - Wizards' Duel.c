6fd45dd8f73ceed45dcbf1d2b7ed0ddac5fa175d
sngr0x0 <136003366+Frances-co@users.noreply.github.com>
2024-11-09 13:52:34 +0200

    Update 591A - Wizards' Duel.c

--- 591A - Wizards' Duel.c
+++ 591A - Wizards' Duel.c
@@ -1,5 +1,12 @@
 //https://codeforces.com/problemset/problem/591/A
 
+//The trick is, the position of the first clash is the same as the position of the second clash cuz the velcotiy
+//doesn't change...
+//If I run from point A with velocity= 3  m/s, I will reach to a specific point..
+//If I went back to A & back again tried to run with 3 m/s in same time t, I'll reach exactly the same point..
+//That's bcs time and velocity doesn't change overtime.
+//So the solution will be the position of the first collision and the formula for it is in the jpg file.
+
 #include <stdio.h>
  
 int main() {
